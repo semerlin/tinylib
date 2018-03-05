133b6361eaa8592358198d746a88043ea02ae9a5
huang yang <elious.huang@gmail.com>
2018-03-05 08:58:28 +0800

    add platform definition code

---
+++ platform/ttypes.h
@@ -0,0 +1,53 @@
+/**
+ * This file is part of the tlog Library.
+ *
+ * Copyright 2017, Huang Yang <elious.huang@gmail.com>. All rights reserved.
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+#ifndef TTYPES_H_
+#define TTYPES_H_
+
+#include "tconfig.h"
+#include "tmacros.h"
+
+T_BEGIN_DECLS
+
+/* common types */
+typedef char   tchar;
+typedef short  tshort;
+typedef long   tlong;
+typedef int    tint;
+typedef tint   tbool;
+
+typedef unsigned char   tuchar;
+typedef unsigned short  tushort;
+typedef unsigned long   tulong;
+typedef unsigned int    tuint;
+
+typedef float   tfloat;
+typedef double  tdouble;
+
+/* boolean definition */
+#ifndef	FALSE
+#define	FALSE	(0)
+#endif
+
+#ifndef	TRUE
+#define	TRUE	(!FALSE)
+#endif
+
+
+#ifndef NULL
+#ifdef __cplusplus
+    #define NULL    (0L)
+#else
+    #define NULL    ((void*)0)
+#endif
+#endif
+
+#define UNUSED(x)   ((void)(x))
+
+T_END_DECLS
+
+#endif /* TTYPES_H_ */
